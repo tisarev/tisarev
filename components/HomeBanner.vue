2c3858c4ce20665242bc48b8c492021dd542fb62
tisarev <tisarev@gmail.com>
2022-07-10 23:46:27 +0400

    5

--- components/HomeBanner.vue
+++ components/HomeBanner.vue
@@ -1,5 +1,20 @@
 <template>
     <section class="home-banner">
         <TheHeader />
+        <div class="container">
+            <div class="home-banner__text">
+                <h1>Создаю <b>веб-дизайн</b>, <br>разрабатываю <b>сайты для бизнеса</b>, <br><b>презентации</b> и <b>2D/3D графику</b> <br>для интерфейсов</h1>
+                <ul>
+                    <li>
+                        <div class="big">15+</div>
+                        Опыт<br>работы
+                    </li>
+                    <li>
+                        <div class="big">300+</div>
+                        Выполненных<br>работ
+                    </li>
+                </ul>
+            </div>
+        </div>
     </section>
 </template>
